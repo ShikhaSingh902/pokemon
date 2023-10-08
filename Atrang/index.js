27b8e13d54532d263e89f77cae78e1ca7f49f43c
Knivu <129518812+Knivu@users.noreply.github.com>
2023-10-08 19:32:49 +0530

    Add files via upload

---
+++ Atrang/index.js
@@ -0,0 +1,16 @@
+
+function reg(){
+    alert("aahladh is over");
+}
+
+
+function w_des(){
+    let caption = document.getElementById("caption");
+    caption.innerText="by IIITT";
+}
+function w_def(){
+    let caption = document.getElementById("caption");
+    caption.innerText="PHOTO OF THE MONTH";
+}
+
+window.onload = onstart;
